553240464beefbe81102a42c529671237d136386
Ashutosh <ashutoshkmr419@gmail.com>
2024-04-22 14:17:52 +0530

    modified

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { deleteUser, fetchData, fetchIdData, loginUser, signupUser, updateUser, user } from "../controller/user.controller.js";
+import { auth } from "../middleware/auth.middleware.js";
 
 
 const router = Router();
+
+router.use(auth)
 
 router.route("/register").post(signupUser)
 router.route("/login").post(loginUser)
