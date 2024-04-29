7e9055bd9187f2feb649aae101ab597749966f39
Ashutosh <ashutoshkmr419@gmail.com>
2024-04-29 15:56:04 +0530

    modified

--- backend/src/model/Signup.model.js
+++ backend/src/model/Signup.model.js
@@ -30,39 +30,39 @@ const signupSchema = new Schema({
 },{timestamps:true});
 
 
-// signupSchema.pre("save",async function(next){
-//     if(!this.isModified("password")) return next();
-//     this.password = bcrypt.hash(this.password,10)
-//     next()
-// })
+signupSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
 
 
-// signupSchema.methods.isPasswordCorrect = async function (password) {
-//     return await bcrypt.compare(password,this.password)
-// }
+signupSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
 
-// signupSchema.methods.generateAccessToken = function (){
-//     return jwt.sign({
-//         _id: this._id,
-//         firstname: this.firstname,
-//         lastname: this.lastname,
-//         username: this.username,
-//         email: this.email
-//     },
-//     process.env.ACCESS_TOKEN_SECRET,
-//     {
-//         expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-//     }
-// )
-// }
-// signupSchema.methods.generateRefreshToken = function (){
-//     return jwt.sign({
-//         _id: this._id
-//     },process.env.REFRESH_TOKEN_SECRET,
-//     {
-//         expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-//     }
-// )
-// }
+signupSchema.methods.generateAccessToken = function (){
+    return jwt.sign({
+        _id: this._id,
+        firstname: this.firstname,
+        lastname: this.lastname,
+        username: this.username,
+        email: this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+signupSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id: this._id
+    },process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const Signup = mongoose.model("Signup",signupSchema)
